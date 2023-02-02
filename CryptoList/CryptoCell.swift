361c8bc4d275e7bb40c4c77dca4007bf6a59333e
Gonzalo Trujillo Vallejo <4bctrujillog1112@gmail.com>
2023-02-02 16:53:48 +0100

    Interfece changed

--- CryptoList/CryptoCell.swift
+++ CryptoList/CryptoCell.swift
@@ -12,31 +12,21 @@ class CryptoCell: UITableViewCell {
     
     @IBOutlet weak var cryptoNameLabel: UILabel!
     @IBOutlet weak var cryptoPriceLabel: UILabel!
-    @IBOutlet weak var cryptoImage: UIImageView!  
+    @IBOutlet weak var cryptoImage: UIImageView!
     
     let identifier = "CryptoCell"
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        
-        // Personalización de la apariencia de la celda
-        self.backgroundColor = UIColor.gray
-        self.layer.cornerRadius = 10
-        self.layer.borderWidth = 1
-        self.clipsToBounds = true
-        
         // Personalización de los labels
-        cryptoNameLabel.textColor = UIColor.purple
-        cryptoPriceLabel.textColor = UIColor.purple
-        
-        // Personalización de la imagen
-        cryptoImage.layer.cornerRadius = cryptoImage.frame.width / 2
-        cryptoImage.clipsToBounds = true
-    }
+        cryptoNameLabel.textColor = UIColor.blue
+        cryptoPriceLabel.textColor = UIColor.blue
 
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
