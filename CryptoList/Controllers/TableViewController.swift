0a2aa2752d01f44809247230fd6dc9dec0bb950f
Gonzalo Trujillo Vallejo <4bctrujillog1112@gmail.com>
2023-02-03 13:42:30 +0100

    Mardkdowns added

--- CryptoList/Controllers/TableViewController.swift
+++ CryptoList/Controllers/TableViewController.swift
@@ -9,17 +9,15 @@ import UIKit
 
 
 class TableViewController: UITableViewController, UISearchResultsUpdating {
-
+    
     //Lista de CryptoMonedas
     var cryptoCurrencyList =  [CryptoModel]()
-    
     //Array donde se van a guardar los resultados encontrados en la lista de cryptomonedas
     var filteredCryptoCurrencyList = [CryptoModel]()
-    
     let cryptoManager = CryptoManager()
     //Instancia de la barra de búsqueda
     let searchController = UISearchController(searchResultsController: nil)
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -34,21 +32,14 @@ class TableViewController: UITableViewController, UISearchResultsUpdating {
         tableView.register(UINib(nibName: CryptoCell.identifier, bundle: nil), forCellReuseIdentifier: CryptoCell.identifier)
         
         // Configurar la barra de búsqueda
-        // Asigna la clase como el delegado encargado de actualizar los resultados de búsqueda
         searchController.searchResultsUpdater = self
-
-        // Establece que el fondo detrás del controlador de búsqueda no se oscurecerá durante la presentación
         searchController.obscuresBackgroundDuringPresentation = false
-
-        // Establece el texto placeholder que aparecerá en la barra de búsqueda
         searchController.searchBar.placeholder = "Buscar CryptoMonedas"
-
-        // Asigna el controlador de búsqueda al elemento de navegación
         navigationItem.searchController = searchController
-
-        // Especifica que el contexto de presentación se define en esta clase
         definesPresentationContext = true
     }
+    
+//MARK: - TableView methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return filteredCryptoCurrencyList.count
@@ -74,10 +65,12 @@ class TableViewController: UITableViewController, UISearchResultsUpdating {
         
         return cell
     }
-
+    
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100
     }
+    
+    //MARK: - SearchBar methods
     
     func updateSearchResults(for searchController: UISearchController) {
         if let searchText = searchController.searchBar.text, !searchText.isEmpty {
@@ -90,6 +83,6 @@ class TableViewController: UITableViewController, UISearchResultsUpdating {
         
         self.tableView.reloadData()
     }
-
+    
 }
 
